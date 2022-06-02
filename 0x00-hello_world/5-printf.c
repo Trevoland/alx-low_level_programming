8ea62bee29df651d93fc0132a8e7e92524c7ac0a
trevoland nagoor <trevolandn26@gmail.com>
2022-06-02 03:37:58 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My hello, printf task
+ *Return: 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+}
