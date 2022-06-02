91d38a2409f4500f84856c4b4dbb313199c6987f
trevoland nagoor <trevolandn26@gmail.com>
2022-06-02 03:14:42 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  *Return: 0 after printing the function
  */
-int main(void)
-[
-puts("\"Programming is the building a multilingual puzzle":
+int main(void) 
+{
+puts("\"Programming is like buliding a multilingual puzzle");
 return (0);
-] 
+} 
