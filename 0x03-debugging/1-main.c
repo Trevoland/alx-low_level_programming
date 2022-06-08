53ead292065031e9b04a980b7065efae601be44a
trevoland nagoor <trevolandn26@gmail.com>
2022-06-08 01:45:45 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
- /**
+
+/**
  * main - causes an infinite loop
  * Return: 0
  */
@@ -11,14 +12,13 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
- /*
- *while (i < 10)
- *{
- *               putchar(i);
- *}
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
  */
-printf("Infinite loop avoided! \\0/n");
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
