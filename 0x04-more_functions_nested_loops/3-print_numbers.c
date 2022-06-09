7ea09eb5955125736ffd00b6683a6954fb0d3e26
Trevoland <104766934+Trevoland@users.noreply.github.com>
2022-06-09 13:09:53 +0200

    3-prints_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers
- * Return: Always 0.
+ * print_most_numbers - prints numbers
+ * Return:0
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
-char n = 0;
+	char n = 0;
 
-while (n <= 9)
-{
-_putchar ('0' + n);
-n++;
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
 
-}
-putchar ('\n');
+		n++;
+	}
+	_putchar('\n');
 
 }
