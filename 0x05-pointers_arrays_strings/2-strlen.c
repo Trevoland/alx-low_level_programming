71f6e8984c53948dff28e6048c0cc5787392a005
trevoland nagoor <trevolandn26@gmail.com>
2022-06-13 01:13:43 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,14 @@
  * _strlen -length of a string
  * @s: A pointer to an int that will be changed/updated
  *
- * Return: void that means our answer is correct
+ * Return; void that means our answer is correct
  */
 
 int _strlen(char *s)
 {
 int u;
 
-u= 0;
+u = 0;
 while (s[u] != '\0')
 {
 u++;
