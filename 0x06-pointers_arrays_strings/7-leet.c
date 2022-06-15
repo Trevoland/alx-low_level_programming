b1858ef2e39038cf0ee7c72a4f2b68e574daa3df
trevoland nagoor <trevolandn26@gmail.com>
2022-06-15 10:38:21 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,8 +8,8 @@
 char *leet(char *s)
 {
 int count = 0, i;
-int low_letters[] = {97, 101, 111, 116, 108};
-int upp_letters[] = {65, 69, 79, 84, 76};
+int lower_case[] = {97, 101, 111, 116, 108};
+int upper_case[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
 
 
@@ -17,9 +17,9 @@ while (*(s + count) != '\0')
 {
 for (i = 0; i < 5; i++)
 {
-if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
 {
-*(s + count) = number[i];
+*(s + count) = numbers[i];
 break;
 }
 }
