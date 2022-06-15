b2bef4afd71a3ea2b1b10e5a453fc920e926a360
trevoland nagoor <trevolandn26@gmail.com>
2022-06-15 09:52:58 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,8 +20,8 @@ for (i = 0; i < 13; i++)
 {
 if (*(s + count) == separators[i])
 {
-if ((*(s + (count + 1)) >=97 ) && (*(s + (count + 1)) <= 122))
-*(s + (count + 1)) = *(s + (count + 1 )) - 32;
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
 break;
 }
 }
