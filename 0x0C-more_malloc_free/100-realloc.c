7afffc73ef0ba19ff9403fd9a4cf6173ce39ed73
Trevoland <104766934+Trevoland@users.noreply.github.com>
2022-06-30 12:18:12 +0200

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
+ * _realloc -  reallocates a memory block using malloc and free
  * @ptr: pointer
  * @old_size: old size
  * @new_size: new size
  * Return: pointer
  */
-void *realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *clone, *relloc;
 	unsigned int i;
