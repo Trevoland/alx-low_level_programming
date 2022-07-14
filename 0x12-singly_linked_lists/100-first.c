da84bbe94a2fc14d7f1a50d7e5a4e59eadc949bb
Trevoland <104766934+Trevoland@users.noreply.github.com>
2022-07-14 12:04:50 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,8 @@
  * Return: no return.
  */
 
-void __attribute__
-((constructor)) bmain()
+void __attribute__((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow");
-	printf(".\n I bore my house upon my black!\n");
+    printf("You're beat! and yet, you must allow");
+    printf(",\nI bore my house upon my back!\n");
 }
